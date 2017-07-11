3d8a9dcfb9337ce2f686573d5e03f2a4677ed5c6
Kyle <kyle.rits.rodgers@gmail.com>
2017-07-11 10:56:25 -0400

    Created a rough outline for the app.

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,78 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1></h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+
+  <main id="app">
+
+    <!-- Header Area -->
+    <header>
+
+      <!-- Logo -->
+      <div class="logo">
+        <h2>Artisan</h2>
+        <p>
+          <small>curated by <a href="https://reddit.com/r/artisanvideos">r/ArtisanVideos</a></small>
+        </p>
+      </div>
+
+      <!-- Navigation -->
+      <div class="navigation">
+
+        <!-- Action Buttons -->
+        <ul class='action-buttons'>
+          <li><button type="button" name="search" class="nav-btn">Search</button></li>
+          <li><button type="button" name="open" class="nav-btn">Open</button></li>
+        </ul>
+
+        <div id="aside">
+          <section class="categories">
+            <!-- List of Different Categories -->
+          </section>
+          <section class="social">
+            <!-- Links to my social media and github -->
+          </section>
+        </div>
+
+      </div>
+
+    </header>
+
+    <!-- Main Area -->
+    <div class="container">
+
+      <!-- Player Section -->
+      <section id="player">
+
+        <div class="section-header">
+          <!-- Initial Message -->
+          <h2>Play a Random Video</h2>
+          <!-- Plays a random video from the list. Or the first one. It's not really to important which. -->
+          <button type="button" name="playRandom" class="btn">Play</button>
+        </div>
+
+        <!-- Video Player (Initialized either when 'random' is selected or a video is selected from the list.) -->
+
+      </section>
+
+      <!-- List Section -->
+      <section id="listing">
+
+        <div class="section-header">
+          <h2>All</h2>
+        </div>
+
+        <!-- List of Videos to Play -->
+
+      </section>
+
+    </div>
+
+    <!-- Footer Area -->
+    <footer>
+
+    </footer>
+
+  </main>
+
+
 </template>
 
 <script>
@@ -31,30 +87,10 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1, h2 {
-  font-weight: normal;
-}
+  #app {
+    font-family: 'Alias', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
